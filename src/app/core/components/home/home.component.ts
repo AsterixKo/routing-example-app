ca11c868ed14d295cb6eea9b75b2a08f06b04755
MSI\moneo <jvr.moneo@gmail.com>
2020-10-21 12:34:58 +0200

    escribimos los objetos de momento sin servicios

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  publications: Object[];
+  constructor() {
+    this.publications = [
+      { name: 'Pepe', publication: 'Esta es la publicación de Pepe' },
+      { name: 'Maria', publication: 'Esta es la publicación de Maria' }];
+  }
 
   ngOnInit(): void {
   }
